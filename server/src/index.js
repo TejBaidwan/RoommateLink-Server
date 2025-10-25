82207266df54b9f6105a8628b19bf80f07b60484
Tej Baidwan <w0806279@myscc.ca>
2025-10-25 16:38:23 -0400

    Added the emailQueue to the main app

--- server/src/index.js
+++ server/src/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
+import "./services/emailQueue.js"
 
 dotenv.config();
 const app = express();
